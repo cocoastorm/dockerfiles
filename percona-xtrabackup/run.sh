43aba89a2655adb269c7b719a27d68add8cbc954
Khoa <khoa.tan.nguyen.96@gmail.com>
2017-05-01 16:50:22 -0700

    remove unused script and text files

--- percona-xtrabackup/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Generate Encryption Key for innobackupex, if none is found
-if [ ! -f /backups/mysql/encryption_key ]; then
-  printf '%s' "$(openssl rand -base64 24)" | tee /backups/mysql/encryption_key && echo
-fi
